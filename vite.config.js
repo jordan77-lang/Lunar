d9c86d3b07989cc5e70aaa8c3c19e3149719cbad
Mark Jordan <jordan77@asu.edu>
2026-02-17 16:35:23 -0700

    asas

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/Lunar/', // GitHub Pages repository name
+  build: {
+    outDir: 'docs',
+  },
   server: {
     host: true, // Open to local network
   }
